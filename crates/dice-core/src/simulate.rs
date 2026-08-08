1dbff5eaadc3ae5d956e802292111ed9b14fd25c
Alex Kennedy <alex@alexkennedy.dev>
2026-08-08 14:07:21 +0900

    Add basic tests to simulate.

--- crates/dice-core/src/simulate.rs
+++ crates/dice-core/src/simulate.rs
@@ -22,3 +22,38 @@ pub fn simulate_expression(expr: &Expr) -> i32 {
     }
   }
 }
+
+#[cfg(test)]
+mod tests {
+  use crate::parser::parse;
+  use rstest::rstest;
+
+  use super::*;
+
+  #[rstest]
+  #[case("1+1", 2)]
+  #[case("1*1", 1)]
+  #[case("1*-1", -1)]
+  #[case("-1*-1", 1)]
+  #[case("(2*2)+1", 5)]
+  #[case("2*2+1", 5)]
+  #[case("2*(2+1)", 6)]
+  #[case("2*(((((((((((2+1)))))))))))", 6)]
+  #[case("1*1*1*1*1*1*1*1*1*1", 1)]
+  #[case("-1", -1)]
+  #[case("((((1+(2+(3+4+5))))+((+6+--7)+(8+9))))", 1+2+3+4+5+6+7+8+9)]
+  fn evaluates_constant_expression_correctly(#[case] expr: &str, #[case] result: i32) {
+    let parsed = parse(expr).expect("failed to parse");
+    assert_eq!(simulate_expression(&parsed), result);
+  }
+
+  #[rstest]
+  #[case("2*d6", 2, 12)]
+  #[case("-d6", -6, -1)]
+  #[case("d2+d2+d2+d2+d2+(d2+d2)", 7, 14)]
+  fn dice_term_is_within_bounds(#[case] expr: &str, #[case] min: i32, #[case] max: i32) {
+    let parsed = parse(expr).expect("failed to parse");
+    assert!(simulate_expression(&parsed) >= min);
+    assert!(simulate_expression(&parsed) <= max);
+  }
+}
